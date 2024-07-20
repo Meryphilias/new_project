4f63c575a70e7fc31b472b9eaa697a1600173b3d
Mery <kgs246531@gmail.com>
2024-07-21 07:40:32 +0900

    add background

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
     const title = document.getElementById('title');
+    const login = document.getElementById('login');
+    const signup = document.getElementById('signup');
     title.addEventListener('click', function(){
         window.location.href = "https://naver.com";
+    });
+    login.addEventListener('click', function(){
+        window.location.href = "/login";
+    });
+    signup.addEventListener('click', function(){
+        window.location.href = "/signup";
     });
 });
